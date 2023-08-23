a6d367cb19a1fc2ad7124d8d0bc139930aeb29e2
kirill <glad-web-developer@gmail.com>
2023-08-23 23:20:53 +0900

    init

--- backend/kenguru/api/GetStatusApi.py
+++ backend/kenguru/api/GetStatusApi.py
@@ -11,20 +11,20 @@ from kenguru.serializers import LocalSettingSerializer
 
 class GetStatusApi(APIView):
     def get(self, request):
+        """
+        когда стучаться из вне поверят что есть звук
+        """
 
         try:
             local_setting = LocalSetting.objects.last()
-        except Exception:
-            local_setting = LocalSetting()
-
-
-        audio_list = soundcard.all_speakers()
-        audio_list = list(map(lambda x : x.name, audio_list))
-        audio = request.query_params['audio']
-        if audio in audio_list:
-            return Response("ok")
-        else:
+            audio_list = soundcard.all_speakers()
+            audio_list = list(map(lambda x: x.name, audio_list))
+            if local_setting.audio in audio_list:
+                return Response("ok")
             return Response("Нет устройства")
+        except Exception:
+            return Response("Не настроен клиент")
+
 
 
 
